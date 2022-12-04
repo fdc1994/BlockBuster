96bf2c3d2b675e8232088c753926cfd3d1a468e7
Fabio <fdc1994@hotmail.com>
2022-12-04 17:13:06 +0000

    Add project files.

---
+++ BlockBuster/FormHome.cs
@@ -0,0 +1,15 @@
+namespace BlockBuster
+{
+    public partial class FormHome : Form
+    {
+        public FormHome()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
